9112c11ccc1c5124bbef8711338ec635291ec1d7
alhaw <alhaw@DESKTOP-89ESU8J>
2021-10-05 14:25:25 -0500

    testing commit

--- personal-project-Alharthi/src/Myservlet_Alharthi.java
+++ personal-project-Alharthi/src/Myservlet_Alharthi.java
@@ -27,7 +27,7 @@ public class Myservlet_Alharthi extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("hello world");
+		response.getWriter().append("Hello World");
 	}
 
 	/**
